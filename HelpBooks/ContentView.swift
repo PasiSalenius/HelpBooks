a512cb3125410c8c9ef0ca8317999f4fa4dcaff1
Pasi Salenius <pasi@freshbits.fi>
2026-01-10 15:24:52 +0200

    Initial Commit

---
+++ HelpBooks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelpBooks
+//
+//  Created by Pasi Salenius on 10.1.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelpBooks/HelpBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpBooksApp.swift
+//  HelpBooks
+//
+//  Created by Pasi Salenius on 10.1.2026.
+//
+
+import SwiftUI
+
+@main
+struct HelpBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
